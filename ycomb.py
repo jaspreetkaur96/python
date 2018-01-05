19d362fffcc8acd0dbaf14e5a35c745ad71e91a5
Jaspreet Kaur <jaspreet.k.c1996@gmail.com>
2018-01-05 23:29:44 +0530

    Update ycomb.py

--- ycomb.py
+++ ycomb.py
@@ -19,14 +19,13 @@ with open('data.json', 'w') as outfile:
 #Python is to use store your data in a dict object, which can contain other nested dicts, arrays, booleans, or other primitive types like integers and strings. You can find a more detailed list of data types supported here.
 #json.dumps, which returns the actual JSON string instead of sending it directly to a writable object. This can give you some more control if you need to make some changes to the JSON string (like encrypting it, for example).
 
-connection = pymongo.MongoClient("mongodb://27017")
-db=connection.a
-record1 = db.a_connection 
-#parsed=json.dump(data, json.loads(data.read()))
-p=open('data.json','r')
-parsed=json.loads(p.read())
+#connection = pymongo.MongoClient("mongodb://27017")
+#db=connection.a
+#record1 = db.a_connection 
+#p=open('data.json','r')
+#parsed=json.loads(p.read())
 
-for item in parsed :
- record1.insert(item)
+#for item in parsed :
+ #record1.insert(item)
 
 #print x.string,",",x.get("href")
